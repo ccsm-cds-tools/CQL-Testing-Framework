12543b9f455b4146667855f31d1c0d6595993b2e
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2024-11-21 16:58:07 -0500

    add Mocha cql library code coverage reporter

--- src/buildTestSuite.js
+++ src/buildTestSuite.js
@@ -5,6 +5,7 @@ const fhir = require('cql-exec-fhir');
 const {expect} = require('chai');
 const hooksExporter = require('./exporters/hooks');
 const postmanExporter = require('./exporters/postman');
+const coverageExporter = require('./exporters/coverage');
 
 function buildTestSuite(testCases, library, codeService, fhirVersion, config) {
   const identifier = library.source.library.identifier;
@@ -34,7 +35,7 @@ function buildTestSuite(testCases, library, codeService, fhirVersion, config) {
   // Use describe.skip if the suite has skip: true, use describe.only if the suite has only: true, 
   // otherwise use describe
   const describeFn = config.get('skip') ? describe.skip : config.get('only') ? describe.only : describe;
-  describeFn(libraryHandle, () => {
+  describeFn(libraryHandle, function() {
     let patientSource;
     before('Initialize FHIR patient source', () => {
       switch (fhirVersion) {
@@ -80,6 +81,12 @@ function buildTestSuite(testCases, library, codeService, fhirVersion, config) {
       });
     }
 
+    let coverageReport;
+    before('Initialize coverage report', () => {
+      coverageReport = coverageExporter.initCoverageReport(library.source.library, dumpResultsPath);
+      this.coverageReport  = coverageReport;
+    });     
+
     afterEach('Reset the patient source', () => patientSource.reset());
 
     for (const testCase of testCases) {
@@ -108,6 +115,7 @@ function buildTestSuite(testCases, library, codeService, fhirVersion, config) {
             fs.writeFileSync(filePath, JSON.stringify(results, null, 2), 'utf8');
           }
           const patientId = testCase.bundle.entry[0].resource.id;
+          coverageExporter.addLocalIdResultMap(results.localIdPatientResultsMap, patientId, coverageReport);
           expect(results.patientResults[patientId]).to.exist;
           for (const expr of Object.keys(testCase.expected)) {
             checkResult(expr, results.patientResults[patientId][expr], testCase.expected[expr]);
---
+++ src/exporters/coverage.js
@@ -0,0 +1,105 @@
+// custom-end-reporter.js
+const Mocha = require('mocha');
+const fs = require('fs');
+const path = require('path');
+
+class LibraryCoverageReporter extends Mocha.reporters.Base{
+  constructor(runner) {
+    super(runner);
+
+    let currentSuite = null;
+
+    runner.on(Mocha.Runner.constants.EVENT_SUITE_BEGIN, (suite) => {
+      currentSuite = suite;
+    });
+
+    runner.on(Mocha.Runner.constants.EVENT_SUITE_END, () => {
+      if(currentSuite.coverageReport){
+        const cr = currentSuite.coverageReport;
+        console.log(`${cr.library.id}: ${cr.covered}/${cr.total} ${100 * (cr.covered/cr.total)}%`);
+        const jsonOutput = JSON.stringify(cr, null, 2);
+        // only write file if dumpResultsPath set, otherwise just write summary to console
+        if(cr.resultsPath){
+          const filePath = path.join(cr.resultsPath, `${cr.library.id}.coverage.json`);
+          fs.writeFileSync(filePath, jsonOutput, 'utf8');
+        }        
+      }
+    });
+  }
+
+  static initCoverageReport(librarySource, dumpResultsPath) {
+    const coverageReport = {
+      library: librarySource.identifier,
+      resultsPath: dumpResultsPath
+    };
+    coverageReport.uncoveredExpressions = this.extractLocalIdData(librarySource.statements.def);
+    coverageReport.total = Object.keys(coverageReport.uncoveredExpressions).length;
+    coverageReport.covered = 0;
+    return coverageReport;
+  }
+  
+  static extractLocalIdData(jsonArray) {
+    const resultObject = {};
+  
+    function recursiveSearch(obj) {
+      if (typeof obj !== 'object' || obj === null) {
+        return;
+      }
+  
+      // Check if the current object has a 'localId' property
+      if (obj.hasOwnProperty('localId')) {
+        const localId = obj.localId;
+        const extractedData = {};
+  
+        // Iterate over the properties of the object
+        for (const key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            const value = obj[key];
+            // Check if the value is a literal (not an object or array)
+            if (value !== null && typeof value !== 'object') {
+              extractedData[key] = value;
+            }
+          }
+        }
+  
+        // Check if 'locator' and 'type' is not null in the extracted data
+        // Skip types that don't require unit testing - alias, as, type specifiers, literal and property extractors
+        if (extractedData.locator !== undefined && extractedData.type !== undefined &&
+          extractedData.alias === undefined &&
+          (extractedData.type !== 'As' && !extractedData.type.includes('TypeSpecifier') && extractedData.type !== 'Literal' && extractedData.type !== 'Property')
+        ) {
+          resultObject[localId] = extractedData;
+        }
+      }
+  
+      // Recursively search through all properties of the object, skipping signature and resultTypeSpecifier
+      for (const key in obj) {
+        if (obj.hasOwnProperty(key) && key !== 'signature' && key !== 'resultTypeSpecifier') {
+          recursiveSearch(obj[key]);
+        }
+      }
+    }
+  
+    // Iterate over each JSON object in the array
+    jsonArray.forEach(jsonObject => {
+      recursiveSearch(jsonObject);
+    });
+  
+    return resultObject;
+  }
+  
+  static addLocalIdResultMap(testCaseResults, patientId, coverageReport) {
+    // Iterate over each key in the results map
+    const keys = Object.keys(testCaseResults[patientId][coverageReport.library.id]);
+    for (const key of keys) {
+      // Check if the key exists in the expressions map
+      if (coverageReport.uncoveredExpressions.hasOwnProperty(key)) {
+        // Remove the key from the expressions map
+        delete coverageReport.uncoveredExpressions[key];
+        coverageReport.covered += 1;
+      }
+    }
+  }
+}
+
+module.exports = LibraryCoverageReporter;
